9997a751911cda387bf69a71dfea9abca1424fd5
janosiegle <arumbaja@gmail.com>
2019-11-30 11:25:55 +0100

    - add smal dgm

--- Lech/RData/R.R
+++ Lech/RData/R.R
@@ -2,7 +2,7 @@ setwd("D:/Uni/Lech/tamarix/Lech/RData")
 
 list.files()
 
-T <- read.csv("tges.csv")
+T <- read.csv("tges.csv", stringsAsFactors = FALSE)
 
 ## pnt.live über pnt.vit ☻
 
@@ -31,3 +31,28 @@ lines(2:5, medi[, 4], col = "blue", lwd = 2)
 legend(2, 250, c("Q75", "Median", "Q25"), col = c("blue", "red", "darkblue"),
        bty = "n", lwd = 2)
 
+
+## mean dist n = 5 über Vitalität
+
+medi <- matrix(NA, 5, 4)
+colnames(medi) <- c("Class", "Q25", "Median", "Q75")
+medi[, 1] <- j <-2:6
+
+x <- cbind(T[, 20], T[, 22])
+
+for (i in j) {
+  medi[i-1, 3] <- median(x[x[,1] == i, 2])
+  medi[i-1, 2] <- quantile(x[x[,1] == i, 2], probs = c(0.25))
+  medi[i-1, 4] <- quantile(x[x[,1] == i, 2], probs = c(0.75))
+}
+
+plot(x[, 1], x[, 2], xlim = c(2, 6), ylim = c(0, 10),
+     xlab = "", ylab = "NDVI", main = "Group 1 (>870m)", type = "n")
+polygon(c(2:6, 6:2), c(medi[, 4], rev(medi[, 2])),
+        col = "lightgrey", border = NA)
+points(x[, 1], x[, 2], pch = 4)
+lines(2:6, medi[, 3], col = "red", lwd = 2)
+lines(2:6, medi[, 2], col = "darkblue", lwd = 2)
+lines(2:6, medi[, 4], col = "blue", lwd = 2)
+legend(2, 250, c("Q75", "Median", "Q25"), col = c("blue", "red", "darkblue"),
+       bty = "n", lwd = 2)

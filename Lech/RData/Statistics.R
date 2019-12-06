d0da66b2e9e46aefde6cf0a74e342cabfc17c956
janosiegle <arumbaja@gmail.com>
2019-12-06 18:15:38 +0100

    split analysis and prep, fixed factor isue + colnames

--- Lech/RData/Statistics.R
+++ Lech/RData/Statistics.R
@@ -21,14 +21,20 @@ head(tAll, 5)
 summary(tAll)
 str(tAll)
 
+for (i in 2:22) {
+  tAll@data[, i] <- as.character(tAll@data[, i])
+}
+
+for (j in c(3, 5:22)) {
+  tAll@data[, j] <- as.numeric(tAll@data[, j])
+}
+
 ### Lech-Mittellinie
 lech <- readOGR("./Lech/shapes/Lech_Mittellinie.shp")
 
 ### Kontrolle
 plot(lech)
 points(tAll)
-
-
 
 # Distanz der Punkte zur Lech-Mittellinie ####
 tAll_Dist <- cbind(tAll@data, Dist = dist2Line(as.data.frame(tAll)[c(24:25)], lech)[,1])
